c36554af5ec274db1d3537fbe2c8c4be0ab4dfa0
AllenXOD <932037526@qq.com>
2023-09-19 10:56:37 +0800

    dynamic themes

--- dynamic-themes/Index.js
+++ dynamic-themes/Index.js
@@ -3,7 +3,7 @@
  * @Date: 2023-09-18 11:28:37
  * @Email: xudong@adbright.cn
  * @LastEditors: AllenXD
- * @LastEditTime: 2023-09-18 12:11:24
+ * @LastEditTime: 2023-09-19 10:54:17
  * @Description: file information
  * @Company: your company
  */
@@ -14,6 +14,9 @@ const store = reactive({
   theme: 0,
   changeTheme: () => {
     store.theme = store.theme === 0 ? 1 : 0
+  },
+  mounted: ($el) => {
+    document.querySelector(':root').style.setProperty('--button-color', $el.dataset.color)
   }
 })
 
